a061ef45a4e3c8b8fa678a6a23624965549c1e4d
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-05-13 15:31:17 +0200

    add API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,6 +3,39 @@ import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+import PropTypes from 'prop-types';
+
+const sendOrder = (options, tripCost, tripDetails) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    ...tripDetails,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({options, tripCost, setOrderOption}) => (
   <Row>
@@ -14,8 +47,17 @@ const OrderForm = ({options, tripCost, setOrderOption}) => (
     ))}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
+        <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
       </Col>
   </Row>
   );
+
+  OrderForm.propTypes = {
+    tripCost: PropTypes.string,
+    options: PropTypes.any,
+    setOrderOption: PropTypes.func,
+    name: PropTypes.string,
+    tripDetails: PropTypes.object,
+  };
   
   export default OrderForm;
---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should throw error without required props', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+  
+  const expectedLink = '/trip/abc';
+  const expectedImage = 'image.jpg';
+  const expectedName = 'alt';
+  let expectedTags = ['tag1', 'tag2', 'tag3'];
+  const tripId = 'abc';
+
+  const component = shallow(<TripSummary
+    id={tripId}
+    image={expectedImage}
+    name={expectedName} 
+    tags={expectedTags} />);
+
+  it('should render correct link', () => {
+    const renderedLink = component.find('.link').prop('to');
+    expect(renderedLink).toEqual(expectedLink);
+  });
+
+  it('should render correct image and alt', () => {
+    const renderedImage = component.find('img').prop('src');
+    const renderedAlt = component.find('img').prop('alt');
+    expect(renderedImage).toEqual(expectedImage);
+    expect(renderedAlt).toEqual(expectedName);
+  });
+
+  it('should render tags in correct spans', () => {
+    const renderedTagOne = component.find('.tags span').at(0).text();
+    const renderedTagTwo = component.find('.tags span').at(1).text();
+    const renderedTagThree = component.find('.tags span').at(2).text();
+    expect(renderedTagOne).toEqual(expectedTags[0]);
+    expect(renderedTagTwo).toEqual(expectedTags[1]);
+    expect(renderedTagThree).toEqual(expectedTags[2]);
+  });
+
+  it('should not render div tags if props tags is false', () => {
+    const component = shallow(<TripSummary />);
+    expect(component.find('.tags').exists()).toBe(false);
+  });
+});
